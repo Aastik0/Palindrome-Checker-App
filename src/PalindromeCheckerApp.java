5ea8927ebf13d8eb4be8ab7d94fb8533a56d0305
Aastik0 <aastik7898@gmail.com>
2026-02-25 09:32:48 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class PalindromeCheckerApp {
+
+    private static final String APP_NAME = "Palindrome Checker App";
+    private static final String VERSION = "v1.0";
+
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+
+    private static void displayWelcomeMessage() {
+        System.out.println("=================================");
+        System.out.println(" Welcome to " + APP_NAME);
+        System.out.println(" Version: " + VERSION);
+        System.out.println("=================================");
+    }
+}
